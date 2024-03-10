de617dc33b7f0015f483b0950daa56774a78de3a
Person <135844364+waylon-20@users.noreply.github.com>
2024-03-09 18:43:44 -0600

    Update of STK

---
+++ HOW2UPDATE.md
@@ -0,0 +1,5 @@
+How To Run:
+Type "bash -c "$(wget -q -O- https://github.com/waylon-20/stk/raw/main/updatestk.sh)"
+into your linux terminal.
+:)
+   -waylon-20
---
+++ updatestk.sh
@@ -0,0 +1,8 @@
+echo updating stk (opensource)...
+sudo apt remove supertuxkart
+sudo apt install supertuxkart
+echo running stk...
+supertuxkart
+echo everything should be working.
+echo please give a shoutout if things are not working!
+echo thank you the very best!
